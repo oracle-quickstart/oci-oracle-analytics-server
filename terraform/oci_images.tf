5b60b8117d9ac901d7416637677d0861ea02066a
Amir Massourian <amir.massourian@oracle.com>
2024-07-10 12:04:47 -0700

    OAS 2023 0501
    Deprecating :
    
    OAS 2023 0201 April Update
    OAS 2022 (6.4) 0601 July Update

--- terraform/oci_images.tf
+++ terraform/oci_images.tf
@@ -6,7 +6,7 @@ variable "marketplace_source_images" {
   }))
   default = {
     main_mktpl_image = {
-      ocid = "ocid1.image.oc1..aaaaaaaancly3fffjr64eiraz36e75mmvv4a3lqxkdpf5psxgzh4xcc2luzq"
+      ocid = "ocid1.image.oc1..aaaaaaaa5ehikl56thp7relwpzz3jcqfefdfx3mqbhkrshug2kvchishsrfq"
       is_pricing_associated = true
       compatible_shapes = ["VM.Standard2.1" , "VM.Standard2.16" , "VM.Standard2.2" , "VM.Standard2.24" , "VM.Standard2.4" , "VM.Standard2.8" , "VM.Standard.E3.Flex" , "VM.Standard.E4.Flex"]
     }
